effff6f80415ee3cd21d474a0ac5c0c2b183a20d
Andrea Miglietta <m.andrea96@hotmail.it>
2022-11-06 22:58:27 +0100

    titolo - data - voto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,7 @@
-@dd($movies)
+@foreach ($movies as $movie)
+    <div>
+        <h3>{{ $movie['original_title' ]}}</h3>
+        <p>{{ $movie['date'] }}</p>
+        <p>{{ $movie['vote'] }}</p>
+    </div>
+@endforeach
