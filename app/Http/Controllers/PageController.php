52f7a31d66e5eaa3525e1a443368c1c5769d6b17
Andrea Miglietta <m.andrea96@hotmail.it>
2022-11-06 22:47:46 +0100

    ritornare tutti i film da pagecontroller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\models\Movie;
 
 class PageController extends Controller
 {
     //
 
     public function index(){
+        $movies = Movie::all();
         return 'prova film';
     }
 }
